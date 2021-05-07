300c3e3696eb2e6907da22011fd8913d7bcd07c0
Emily Wang <write2emily@hotmail.com>
2021-05-06 23:42:06 -0400

    delete commented out tests

--- tests.py
+++ tests.py
@@ -13,27 +13,6 @@ class MyTestCase(unittest.TestCase):
         self.app = app.test_client()
         with app.app_context():
             db.initialize_db()
-            #create temp in-memory test database
-            # self.connection = sql.connect(self.db_fd)
-            # cursor = self.connection.cursor()
-            # cursor.execute("DROP TABLE IF EXISTS products")
-            # cursor.execute("CREATE TABLE products (name TEXT, description TEXT, price FLOAT, stock INTEGER, imgpath TEXT)")
-            # cursor.execute("""INSERT INTO products (name, description, price, stock, imgpath) VALUES \
-            #     ('Penne Pasta', 'Our most popular dish...', 21.99, 10, '/static/images/pasta.jpeg'), \
-            #     ('Chicken Noodle Soup', 'A heartwarming soup made with...', 9.99, 10, '/static/images/soup.jpeg'), \
-            #     ('Strawberry Shortcake Cake', 'A delicious slice of cake...', 4.99, 10, '/static/images/cake.jpeg')
-            # """)
-            # cursor.execute("DROP TABLE IF EXISTS transactions")
-            # cursor.execute("CREATE TABLE transactions (timestamp TEXT, productid INTEGER, value INTEGER)")
-            # self.connection.commit()
-
-
-
-    # def test_web_app_running(self):
-    #     try:
-    #         r = self.app.get("http://127.0.0.1:5000/")
-    #     except:
-    #         self.fail("Could not open web app. Not running, or crashed. Test Failed")
 
     #test routes load successfully
     def test_home_route(self):
